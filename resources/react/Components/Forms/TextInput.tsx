a9f27d0681373c11dac596d04589323eb674134b
Adrian Calton <accalton@gmail.com>
2026-02-03 23:28:12 +1100

    Get forms working with sanctum

--- resources/react/Components/Forms/TextInput.tsx
+++ resources/react/Components/Forms/TextInput.tsx
@@ -1,15 +1,21 @@
 interface Props {
     autofocus?: boolean,
+    label: string,
     name: string,
-    label: string
+    update: Function,
+    value?: string
 }
 
-const TextInput = ({ autofocus = false, name, label }: Props) => {
+const TextInput = ({ autofocus = false, label, name, update, value }: Props) => {
+    const handleChange = (event: React.ChangeEvent<{ name: string, value: string }>) => {
+        update(event.target.name, event.target.value);
+    }
+
     return (
         <div className={'form-input'}>
             <label>
                 {label}:
-                <input type="text" name={name} autoFocus={autofocus} />
+                <input type="text" name={name} value={value} autoFocus={autofocus} onChange={handleChange} />
             </label>
         </div>
     );
--- resources/react/Memberships/Create.tsx
+++ resources/react/Memberships/Create.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Fieldset, Form, Submit, TextInput } from '../Components/Forms';
 
 const Create = () => {
-    const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
-        const formData = new FormData(event.currentTarget);
+    const [formData, setFormData] = useState({
+        name: 'Adrian Calton',
+        email: 'accalton@gmail.com',
+        phone: '0481 500 796'
+    });
+
+    const handleChange = (field: string, value: string) => {
+        setFormData({
+            ...formData,
+            [field]: value
+        });
+    }
+
+    const handleSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
         event.preventDefault();
-        fetch('/memberships/store', {
-            method: 'POST',
-            body: formData
-        }).then(response => console.log(response));
+        axios.post('/memberships/store', formData)
+            .then(response => console.log(response));
     }
 
     return (
         <Form handleSubmit={handleSubmit}>
             <Fieldset>
-                <TextInput name={'name'} label={'Full Name'} autofocus />
-                <TextInput name={'email'} label={'Email Address'} />
-                <TextInput name={'phone'} label={'Phone Number'} />
+                <TextInput name={'name'} label={'Full Name'} value={formData.name} update={handleChange} autofocus />
+                <TextInput name={'email'} label={'Email Address'} value={formData.email} update={handleChange} />
+                <TextInput name={'phone'} label={'Phone Number'} value={formData.phone} update={handleChange} />
                 <Submit />
             </Fieldset>
         </Form>
