5d6b2f664ec5450297e0692286f8fc8e281ba310
Adrian Calton <accalton@gmail.com>
2026-02-16 22:13:44 +1100

    update styling and add conditional to change row types

--- resources/react/Memberships/MembershipForm.tsx
+++ resources/react/Memberships/MembershipForm.tsx
@@ -3,13 +3,14 @@ import { Form } from '@components/Forms/Form';
 import DatePicker from '@components/Forms/Inputs/DatePicker';
 import SelectInput from '@components/Forms/Inputs/SelectInput';
 import { useEffect, useState } from 'react';
-import { MembershipFormState } from '../Interfaces';
+import { Member, MembershipFormState } from '../Interfaces';
 import Button from '@components/Forms/Button';
 import TextInput from '@components/Forms/Inputs/TextInput';
 import Repeater from '@components/Forms/Repeater';
 
 const MembershipForm = () => {
-    const [membersLimit, setMembersLimit] = useState<number | false>(false)
+    const [canAddMember, setCanAddMember] = useState<boolean>(false);
+    const [membersLimit, setMembersLimit] = useState<number | false>(false);
     const [state, setState] = useState<MembershipFormState>({
         contacts: [],
         id: null,
@@ -26,15 +27,31 @@ const MembershipForm = () => {
         } else {
             setMembersLimit(false);
         }
+
+        updateCanAddMember();
     }, [state]);
 
-    const addRow = (type: 'contacts' | 'members') => {
+    useEffect(() => {
+        updateCanAddMember();
+    }, [membersLimit]);
+
+    const addRow = (type: 'contacts' | 'members', content: Member = {
+        id: null,
+        name: null,
+        date_of_birth: null,
+        email: null,
+        phone: null,
+    }) => {
         let rows = state[type];
-        rows.push({
-            id: null,
-        });
+        rows.push(content);
 
         setState({...state, [type]: rows});
+    }
+
+    const confirmRemove = (type: 'contacts' | 'members', index: number) => {
+        if (confirm('Are you sure? This action cannot be undone without refreshing the page before submitting.')) {
+            removeRow(type, index);
+        }
     }
 
     const removeRow = (type: 'contacts' | 'members', index: number) => {
@@ -42,6 +59,22 @@ const MembershipForm = () => {
         rows.splice(index, 1);
 
         setState({...state, [type]: rows});
+    }
+
+    const switchRow = (oldType: 'contacts' | 'members', newType: 'contacts' | 'members', index: number) => {
+        const row = state[oldType][index];
+        removeRow(oldType, index);
+
+        addRow(newType, row);
+    }
+
+    const updateCanAddMember = () => {
+        if (membersLimit === false) {
+            setCanAddMember(true);
+        } else {
+            console.log(state.members);
+            setCanAddMember(state.members.length < membersLimit);
+        }
     }
 
     const validateMembershipType = (value: string): boolean => {
@@ -100,10 +133,11 @@ const MembershipForm = () => {
                 {state.members.map((member, index) => (
                     <Fieldset initialState={member} index={index}>
                         <div className="button-controls">
-                            <Button onClick={() => removeRow('members', index)} label={'Remove'} />
+                            <Button onClick={() => switchRow('members', 'contacts', index)} label={'Make Contact'} />
+                            <Button onClick={() => confirmRemove('members', index)} label={'Remove'} />
                         </div>
-                        <TextInput label={'Full Name'} name={'name'} value={member.name} />
-                        <DatePicker label={'Date of Birth'} name={'date_of_birth'} value={member.date_of_birth} />
+                        <TextInput label={'Full Name'} name={'name'} value={member.name} required />
+                        <DatePicker label={'Date of Birth'} name={'date_of_birth'} value={member.date_of_birth} required />
                         <TextInput label={'Email Address'} name={'email'} value={member.email} />
                         <TextInput label={'Phone Number'} name={'phone'} value={member.phone} />
                     </Fieldset>
@@ -119,12 +153,15 @@ const MembershipForm = () => {
                 {state?.contacts.map((contact, index) => (
                     <Fieldset initialState={contact} index={index}>
                         <div className="button-controls">
-                            <Button onClick={() => removeRow('contacts', index)} label={'Remove'} />
+                            {(canAddMember) && (
+                                <Button onClick={() => switchRow('contacts', 'members', index)} label={'Make Member'} />
+                            )}
+                            <Button onClick={() => confirmRemove('contacts', index)} label={'Remove'} />
                         </div>
-                        <TextInput label={'Full Name'} name={'name'} value={contact.name} />
+                        <TextInput label={'Full Name'} name={'name'} value={contact.name} required />
                         <DatePicker label={'Date of Birth'} name={'date_of_birth'} value={contact.date_of_birth} />
-                        <TextInput label={'Email Address'} name={'email'} value={contact.email} />
-                        <TextInput label={'Phone Number'} name={'phone'} value={contact.phone} />
+                        <TextInput label={'Email Address'} name={'email'} value={contact.email} required />
+                        <TextInput label={'Phone Number'} name={'phone'} value={contact.phone} required />
                     </Fieldset>
                 ))}
             </Repeater>
